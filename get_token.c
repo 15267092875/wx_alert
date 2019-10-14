ea73222db522c92b4e752f66803b3e1511c32c0d
15267092875 <42363868+15267092875@users.noreply.github.com>
2019-10-14 09:30:46 +0800

    Update get_token.c
    企业ID 应用secret作为参数

--- get_token.c
+++ get_token.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
 #include <stdlib.h>
 int main(int argc, char *argv[])
 {
-        system("curl -X POST -s 'https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=##corpid##&corpsecret=##corpsecret##' | sed 's/{\"errcode\":0,\"errmsg\":\"ok\",\"access_token\":\"//g' | sed 's/\",\"expires_in\":7200}//g' ");
-	printf("\n");
+        char cmd[50000];
+        sprintf(cmd,"curl -X POST -s 'https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=%s&corpsecret=%s' | sed 's/{\"errcode\":0,\"errmsg\":\"ok\",\"access_token\":\"//g' | sed 's/\",\"expires_in\":7200}//g' ",argv[1],argv[2]);
+        system(cmd);
         return 0;
 }
